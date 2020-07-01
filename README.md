3cbedf6db2c665554f23873486abb31b300b73b4
songlingsl <songling163@163.com>
2020-07-01 21:23:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+ui:pc前端vue-admin-element
+
+admin:pc后端springboot2 mybatis-plus security的rbac
+
+app：微信小程序后端
---
+++ pc-ui/bin/run-web.bat
@@ -0,0 +1,12 @@
+@echo off
+echo.
+echo [ÐÅÏ¢] Ê¹ÓÃ Vue ÔËÐÐ Web ¹¤³Ì¡£
+echo.
+
+%~d0
+cd %~dp0
+
+cd ..
+npm run dev
+
+pause
